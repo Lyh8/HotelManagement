97f46af710b03fd1d05a8d53debdf05cc7fa3692
unknown <970321618@qq.com>
2021-06-16 22:38:10 +0800

    first commit

---
+++ src/hotel/InHotel.java
@@ -0,0 +1,16 @@
+package hotel;
+
+public class InHotel 
+{		
+	
+	 DP dp=new DP();
+	 IData iData=dp;
+		public InHotel(){}
+	public  String in(int roomNo, String name)
+	{
+		return iData.in_Out_Room(roomNo, name);
+	}	
+		
+		
+}
+
